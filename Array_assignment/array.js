2422edf4bccdbc4306184f6be5f325fd589b9071
anugya-svg <anugyajain2003@gmail.com>
2021-06-13 16:00:02 +0530

    solution of 4 ques

---
+++ Array_assignment/array.js
@@ -0,0 +1,27 @@
+function is_array(x){
+    return Array.isArray(x);
+}
+
+function array_clone(x){
+    var p=x;
+    return p;
+}
+function first(x,n=1){
+let a;
+    if(n<0)
+    {
+        a=x.length;
+    }
+    else a=0;
+    return x.slice(a,n);
+}
+function merge(x){
+    return x.join(',');
+}
+
+function mostFreq(x){
+    for(let freq of x)
+    {
+        
+    }
+}
