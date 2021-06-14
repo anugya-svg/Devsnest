16d1a8ad539377aa10625813fc5697a42ace681b
anugya-svg <anugyajain2003@gmail.com>
2021-06-14 12:37:16 +0530

    tha 6 completed

--- Array_assignment/array.js
+++ Array_assignment/array.js
@@ -18,10 +18,49 @@ let a;
 function merge(x){
     return x.join(',');
 }
-
-function mostFreq(x){
-    for(let freq of x)
+//method -1(using two for loops)
+// function mostFreq(x){
+//     let count=0;
+//     let freq=0;
+//     let val;
+//     for(let i of x)
+//     {
+//         count=0;
+//         for(let j of x)
+//         {
+//             if(i==j)
+//             {
+//                 count++;
+//             }
+//         }
+//         if(freq<count)
+//         {
+//             freq=count;
+//             val=i;
+//         }
+//     }
+//     return `${val} (${freq} times)`;
+// }
+// method 2 using 1 loop and map
+function mostFreq(x)
+{
+    let map={};
+    let max=x[0];
+    for(let i of x)
     {
-        
+        if(!map[i])
+        {
+            map[i]=1;
+        }
+        else
+        {
+            map[i]++;
+            if(map[i]>map[max])
+            {
+                max=i;
+            }
+        }
     }
+    return `${max} (${map[max]} times)`;
 }
+
