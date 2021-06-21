50ed7898ad47a801d28c82170d46a6e3fb96b311
anugya-svg <anugyajain2003@gmail.com>
2021-06-21 15:33:13 +0530

    tha 7 and 8

--- object_assignment/index.js
+++ object_assignment/index.js
@@ -12,5 +12,25 @@ for(let obj of library)
 {
     console.log(`${obj.author}\t\t${obj.title}\t\t${obj.readingStatus}`);
 }
-var library = [ { title: 'The Road Ahead', author: 'Bill Gates', libraryID: 1254 }, { title: 'Walter Isaacson', author: 'Steve Jobs', libraryID: 4264 }, { title: 'Mockingjay: The Final Book of The Hunger Games', author: 'Suzanne Collins', libraryID: 3245 }];
+const p=Math.PI;
+function cyl(r=1,h=1)
+{
+    console.log(`volume of a cylinder:${(p*r*r*h).toFixed(4)}`);
+} cyl(3,5);
+let lib= [ { title: 'The Road Ahead', author: 'Bill Gates', libraryID: 1254 }, { title: 'Walter Isaacson', author: 'Steve Jobs', libraryID: 4264 }, { title: 'Mockingjay: The Final Book of The Hunger Games', author: 'Suzanne Collins', libraryID: 3245 }];
+
+lib.sort((a,b)=>{
+    if(a.libraryID < b.libraryID)
+    {
+        return -1;
+    }
+    else
+    {
+        return 1;
+    }
+});
+for (let obj of lib)
+{
+    console.log(`${obj.author}\t\t${obj.title}\t\t${obj.libraryID}`);
+}
 
