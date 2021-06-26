7aa359b421ae3fdbb7965da2c4bc339ca140779f
anugya-svg <anugyajain2003@gmail.com>
2021-06-26 17:25:08 +0530

    day 10

--- memory_game/main.js
+++ memory_game/main.js
@@ -1,16 +1,28 @@
 const clicked=document.querySelectorAll('.memory-card');
+const text=document.querySelector('h1');
 let hasflip=true;
 let firstcard,secondcard;
-let score=0;
-function flipcard()
+let score=30;
+let lockBoard=false;
+text.innerText=`Turns Left:${score}`;
+function restart()
 {
-
-}
 for (let i of clicked)
-{
+{   
     i.addEventListener('click',()=>{
         console.log(i);
+        score--;
+        text.innerText=`Turns Left:${score}`;
+        if(score===0)
+        {
+            if(confirm('GameOver ! You have used 30 moves :(')){
+                window.location.reload();  
+            }
+            
+        }
+       
         i.classList.add('flip');
+        if(lockBoard===true)return;
         if(hasflip===true)
         {
             firstcard=i;
@@ -23,15 +35,19 @@ for (let i of clicked)
             if(firstcard.dataset.val===secondcard.dataset.val)
             {
             console.log('yes');
-            i.removeEventListener('click',flipcard);
-            score++;
+            i.removeEventListener('click',()=>{
+                console.log("removed");
+            });
+            
             }
             else
             {
+                lockBoard=true;
                setTimeout(() => {
                    firstcard.classList.remove('flip');
                    secondcard.classList.remove('flip');
-               }, 700);
+               }, 800);
+               lockBoard=false;
                 console.log('no');
             }
         }
@@ -39,3 +55,11 @@ for (let i of clicked)
     });
 
 }
+}
+(function shuffle() {
+    clicked.forEach(card => {
+      let ramdomPos = Math.floor(Math.random() * 12);
+      card.style.order = ramdomPos;
+    });
+  })();
+  restart();
