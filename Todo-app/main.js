02bc6609cdd91999a16775b46f32cbc0d5149630
anugya-svg <anugyajain2003@gmail.com>
2021-06-30 12:21:17 +0530

    localstorage part done

--- Todo-app/main.js
+++ Todo-app/main.js
@@ -5,10 +5,10 @@ const newdiv=document.createElement('div');
 main.appendChild(newdiv);
 newdiv.classList.add('scroll');
 let k=0;
-btn.addEventListener('click',()=>{
-    let text=content.value;
-    if(text!=""){
-       k++;
+list=[];
+function addElement(text)
+{
+    k++;
     const task=document.createElement('div');
     task.innerText=text;
     newdiv.appendChild(task);
@@ -20,11 +20,42 @@ btn.addEventListener('click',()=>{
     span.appendChild(dlt);
     task.appendChild(span);
     span.classList.add('img');
-    if(k===5)
+    if(k<5)
+    {
+        newdiv.style.height="";
+        newdiv.style.overflowY="";
+    }
+    if(k>=5)
     {
         newdiv.style.height="300px";
         newdiv.style.overflowY="scroll";
         
     }
+    const dltprop=document.querySelectorAll('.fas');
+    for(let i of dltprop)
+    {
+        i.addEventListener('click',()=>{
+            const toBEdeleted=i.parentNode.parentNode;
+            toBEdeleted.style.display='none';
+            console.log(toBEdeleted);
+            k--;
+            localStorage.removeItem(toBEdeleted.innerText);
+        })
+    }       
+}
+for(let i=0;i<localStorage.length;i++)
+{
+    let raw=localStorage.key(i);
+    // let text=localStorage.getItem(raw);
+    addElement(raw);
+
+}
+
+btn.addEventListener('click',()=>{
+    let text=content.value;
+    if(text!=""){
+    localStorage.setItem(text,k);
+    addElement(text);
     }
 })
+
