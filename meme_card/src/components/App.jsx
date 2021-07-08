238204faa2ae0b3a76c99003e267e62b46afcb92
anugya-svg <anugyajain2003@gmail.com>
2021-07-08 21:08:37 +0530

    tha 16.1

---
+++ meme_card/src/components/App.jsx
@@ -0,0 +1,13 @@
+const App=()=>{
+    const Image=()=>{
+        return (
+            <img src="https://www.ohyaaro.com/wp-content/uploads/2021/05/Coronavirus-Funny-Meme-Photos-Akshay-Kumar-Funny-Pics.jpg" alt="meme" />
+        );
+    }
+    return(
+        <div className="app">
+            <Image/>
+        </div>
+    );
+}
+export default App;
---
+++ meme_card/src/components/container.jsx
@@ -0,0 +1,10 @@
+import App from './App'
+import '../design.css'
+const container =()=>{
+    return (
+        <div className="container">
+            <App/>
+        </div>
+    );
+}
+export default container;
